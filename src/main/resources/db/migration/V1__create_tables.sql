503a1c768c93914a0a6060f5d852609dd0cea95a
Cristiano de Sousa Ruas <cristiano.renuncia@gmail.com>
2026-06-02 15:52:55 -0300

    feat: configure Flyway migrations and PostgreSQL integration

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -5,20 +5,3 @@ CREATE TABLE accounts (
     balance NUMERIC(19,2) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE TABLE transactions (
-    id BIGSERIAL PRIMARY KEY,
-    source_account_id BIGINT NOT NULL,
-    target_account_id BIGINT NOT NULL,
-    amount NUMERIC(19,2) NOT NULL,
-    status VARCHAR(30) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT fk_source_account
-        FOREIGN KEY (source_account_id)
-        REFERENCES accounts(id),
-
-    CONSTRAINT fk_target_account
-        FOREIGN KEY (target_account_id)
-        REFERENCES accounts(id)
-);
